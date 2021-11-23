521383a631e0511c15fd05100a0142845ed4acdb
Jen Kennedy <jenken131@gmail.com>
2021-11-23 15:30:27 -0800

    updated the site title and created articles folder and starter articles

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "digital-garden",
+    title: "Digital Garden",
   },
   plugins: [
     "gatsby-plugin-mdx",
