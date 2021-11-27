fac8d26719ed3628c88224f0f07fc6230e5e0e8e
Jen Kennedy <jenken131@gmail.com>
2021-11-27 13:01:26 -0800

    added content to blog posts and queried blog post titles from articles page

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -1,12 +1,29 @@
 import * as React from "react";
 import Layout from "../components/Layout";
+import { graphql } from "gatsby";
 
-const ArticlesPage = () => {
+const ArticlesPage = ({ data }) => {
+  console.log(data);
   return (
     <Layout pageTitle="Articles">
       <h1>Feel free to peruse all the articles in my garden!</h1>
+      <ul>
+        {data.allFile.nodes.map((node) => (
+          <li key={node.name}>{node.name}</li>
+        ))}
+      </ul>
     </Layout>
   );
 };
+
+export const query = graphql`
+  query {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`;
 
 export default ArticlesPage;
