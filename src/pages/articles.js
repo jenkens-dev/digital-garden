46b2c6354c69fe4c1b94ea1377d133171aac8131
Jen Kennedy <jenken131@gmail.com>
2021-11-28 11:43:22 -0800

    update articles graphql query to use allMdx and displayed content with MDXRenderer

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -1,25 +1,33 @@
 import * as React from "react";
 import Layout from "../components/Layout";
 import { graphql } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
 
 const ArticlesPage = ({ data }) => {
   return (
     <Layout pageTitle="Articles">
       <h1>Feel free to peruse all the articles in my garden!</h1>
-      <ul>
-        {data.allFile.nodes.map((node) => (
-          <li key={node.name}>{node.name}</li>
-        ))}
-      </ul>
+      {data.allMdx.nodes.map((node) => (
+        <article key={node.id}>
+          <h2>{node.frontmatter.title}</h2>
+          <p>Posted: {node.frontmatter.date}</p>
+          <MDXRenderer>{node.body}</MDXRenderer>
+        </article>
+      ))}
     </Layout>
   );
 };
 
 export const query = graphql`
   query {
-    allFile {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
       nodes {
-        name
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
+        id
+        body
       }
     }
   }
